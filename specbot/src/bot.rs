b721d5dc72da856238c9c01e22e995c10cd21395
sunho <ksunhokim123@naver.com>
2018-12-04 21:13:04 +0900

    Add specbot

---
+++ specbot/src/bot.rs
@@ -0,0 +1,37 @@
+use serenity::{
+    framework::standard::{StandardFramework},
+    prelude::*,
+    model::{gateway::Ready}
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+pub fn client(token: &str) -> Client {
+    let mut cli = Client::new(token, Handler).expect("Err creating client");
+    cli.with_framework(framework());
+    cli
+}
+
+fn framework() -> StandardFramework {
+    StandardFramework::new()
+    .configure(|c| c
+        .allow_whitespace(true)
+        .on_mention(true)
+        .prefix(">")
+    )
+    .command("ping", |c| c
+        .cmd(ping)
+    )
+}
+
+command!(ping(_ctx, msg, _args) {
+    if let Err(why) = msg.channel_id.say("Pong! : )") {
+        println!("Error sending message: {:?}", why);
+    }
+});
