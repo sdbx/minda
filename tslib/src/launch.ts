f887bd24a80f66e8b1aa143584d2d4d273993061
Craftingmod <alyac1234@gmail.com>
2019-01-07 13:22:31 +0900

    [tslib] minify에서나 쓸만한 문법 OUT

--- tslib/src/launch.ts
+++ tslib/src/launch.ts
@@ -1,12 +1,14 @@
 import { MindaClient } from "./minda/mdclient"
 
-
-(async ()=>{
-    let client = new MindaClient("black")
+async function run() {
+    const client = new MindaClient("black")
     await client.createRoom({
-    name: "hello",
-    black:-1,
-    white:-1,
-    king:-1,
-    rule: "",
-})})()
+        name: "hello",
+        black:-1,
+        white:-1,
+        king:-1,
+        rule: "",
+    })
+}
+
+run()
