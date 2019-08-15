183f3e030200aad31cb0368c1d49cd20b6520ece
SkyLightQP <combbm@naver.com>
2019-08-15 15:55:47 +0900

    [site] Initial site project

---
+++ site/src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const WrapperStyle = styled.div`
+    text-align: center;
+    font-size: 50px;
+`;
+
+const Main: React.FC = () => {
+    return (
+        <WrapperStyle>
+            TEST
+        </WrapperStyle>
+    );
+};
+
+export default Main;
