2f9ea9a7575e557bccc4e14456096c5f4edabb08
SkyLightQP <combbm@naver.com>
2019-08-16 11:52:06 +0900

    [site] rename App.tsx to "Main.tsx"

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import App from './pages/Main';
 
 ReactDOM.render(<App />, document.getElementById('root'));
